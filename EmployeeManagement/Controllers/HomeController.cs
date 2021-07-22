9ef1d6215769fd6c660e6fbb59e2e06b628934bb
Arpit S <arpittempacc@gmail.com>
2021-07-21 22:31:06 -0400

    Details and Index page with bootstrap css

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployee _iEmployee;
+        public HomeController(IEmployee emp)
+        {
+            _iEmployee = emp;
+        }
+        public ViewResult Index()
+        {
+            return View(_iEmployee.GetAllEmployees());
+        }
+
+        public ViewResult Details(int id)
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _iEmployee.GetEmployee(id),
+                PageTitle = "Employee Details"
+            };
+            return View(homeDetailsViewModel);
+        }
+    }
+}
---
+++ EmployeeManagement/Models/EmployeeDetails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class EmployeeDetails : IEmployee
+    {
+        List<Employee> emp;
+        public EmployeeDetails()
+        {
+            emp = new List<Employee>();
+            emp.Add(new Employee() { id = 1, department = "IT", email = "arpit@gmail.com", name = "Arpit" });
+            emp.Add(new Employee() { id = 2, department = "Accounting", email = "arpit2@gmail.com", name = "Arpit2" });
+            emp.Add(new Employee() { id = 3, department = "IT", email = "arpit3@gmail.com", name = "Arpit3" });
+            emp.Add(new Employee() { id = 4, department = "IT", email = "arpit4@gmail.com", name = "Arpit4" });
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return emp;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return emp.FirstOrDefault(x => x.id == id);
+        }
+
+
+    }
+}
